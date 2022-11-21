2c355bfba3e35891323580720280edf1a16e1315
toomas puur <toomas.puur@hammercode.eu>
2022-11-21 16:54:22 +0100

    without express

--- Resources/frontend/js/subscriber.js
+++ Resources/frontend/js/subscriber.js
@@ -1,10 +1,12 @@
 $.subscribe('plugin/swCollapseCart/onMenuOpen', function() {
     setTimeout(function() {
         StateManager.addPlugin('.ivy--express-checkout-btn','ivyExpress');
+        StateManager.addPlugin('.ivy-banner','ivyBanner');
     }, 500);
 });
 $.subscribe('plugin/swCollapseCart/onLoadCartFinished', function() {
     StateManager.addPlugin('.ivy--express-checkout-btn','ivyExpress');
+    StateManager.addPlugin('.ivy-banner','ivyBanner');
 });
 $.subscribe('plugin/swShippingPayment/onInputChanged', function() {
     StateManager.addPlugin('.ivy-banner','ivyBanner');
--- Resources/views/frontend/detail/buy.tpl
+++ Resources/views/frontend/detail/buy.tpl
@@ -2,7 +2,7 @@
 
 {block name="frontend_detail_buy"}
     {$smarty.block.parent}
-    {include file="frontend/ivy_payment_plugin/button.tpl"
+    {include file="frontend/ivy_payment_plugin/banner.tpl"
         addDivClass='buybox--form'
         addButtonClass='buybox--button'
         iviPrice=$sArticle.price
